a30cc46cdda7e5b6a6e68f35408e4c2f592d0d8e
shengbinliu <203003747@qq.com>
2021-04-26 11:41:08 +0800

    feat 倒入vant  如果需要按需倒入则需要在babel中增加配置 全局的话不需要，如果同时配置会冲突

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
 	<div id="app">
 	<p class="p">adsf
 	<span > 哈哈哈</span>
+	
 	</p>
+	<van-button type="primary">主要按钮</van-button>
+	<van-button type="info">信息按钮</van-button>
+	<van-button type="default">默认按钮</van-button>
+	<van-button type="warning">警告按钮</van-button>
+	<van-button type="danger">危险按钮</van-button>
 	</div>
 
 </template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// main.js
+import Vant from 'vant';
+import 'vant/lib/index.css';
 
+Vue.use(Vant);
 Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App)
 }).$mount('#app')
