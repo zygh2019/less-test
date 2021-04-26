238c13925e8a82cef1bfcbc6f0f6eb677e677d87
shengbinliu <203003747@qq.com>
2021-04-26 13:30:29 +0800

    feat 所有的van按钮例子

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
 	<div id="app">
-	<p class="p">adsf
+	<p class="p">adsf</p>
 	<span > 哈哈哈</span>
+	<dev >
+		<ButtonTest></ButtonTest>
+		
+	</dev>
 	
-	</p>
-	<van-button type="primary">主要按钮</van-button>
-	<van-button type="info">信息按钮</van-button>
-	<van-button type="default">默认按钮</van-button>
-	<van-button type="warning">警告按钮</van-button>
-	<van-button type="danger">危险按钮</van-button>
+	
 	</div>
 
 </template>
 
 <script>
+	import ButtonTest from "./components/vant/buttonTest/ButtonTest.vue"
 	export default {
 		name: 'app',
 		components: {
+			ButtonTest
 		}
 		
 	}
---
+++ src/components/vant/buttonTest/ButtonTest.vue
@@ -0,0 +1,40 @@
+<template>
+	<div id="ButtonTest">
+	<van-button type="primary">主要按钮</van-button>
+	<van-button type="info">信息按钮</van-button>
+	<van-button type="default">默认按钮</van-button>
+	<van-button type="warning">警告按钮</van-button>
+	<van-button type="danger">危险按钮</van-button>
+	<van-button plain type="primary">朴素按钮</van-button>
+	<van-button plain type="info">朴素按钮</van-button>
+	<van-button plain hairline type="primary">细边框按钮</van-button>
+	<van-button plain hairline type="info">细边框按钮</van-button>
+	<van-button disabled type="primary">禁用状态</van-button>
+	<van-button disabled type="info">禁用状态</van-button>
+	<van-button loading type="primary" />
+	<van-button loading type="primary" loading-type="spinner" />
+	<van-button loading type="info" loading-text="加载中..." />
+	<van-button square type="primary">方形按钮</van-button>
+	<van-button round type="info">圆形按钮</van-button>
+	<van-button icon="plus" type="primary" />
+	<van-button icon="plus" type="primary">按钮</van-button>
+	<van-button type="primary" size="large">大号按钮</van-button>
+	<van-button type="primary" size="normal">普通按钮</van-button>
+	<van-button type="primary" size="small">小型按钮</van-button>
+	<van-button type="primary" size="mini">迷你按钮</van-button>
+	<van-button color="#7232dd">单色按钮</van-button>
+	<van-button color="#7232dd" plain>单色按钮</van-button>
+	<van-button color="linear-gradient(to right, #ff6034, #ee0a24)"> 渐变色按钮</van-button>
+	</div>
+
+</template>
+
+<script>
+	export default {
+		name: 'ButtonTest',
+		
+	}
+	// 创建构造器
+	
+</script>
+
